3bc4877b852e3670f7f0e4ac6367bfed2fbff42a
Juancabel <jc.beltrang1@uniandes.edu.co>
2024-10-13 21:34:38 -0500

    Healthcheck

--- ofipensiones/urls.py
+++ ofipensiones/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include, path
+import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('cronograma.urls')),
     path('', include('estudiantes.urls')),
-    path('', include('facturas.urls'))
+    path('', include('facturas.urls')),
+    path('health-check/', views.healthCheck)
 ]
